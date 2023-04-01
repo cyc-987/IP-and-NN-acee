993f9a7cd024692b4804b0911263ebdd8c315b44
cyc-987 <2516803593@qq.com>
2023-04-01 18:59:10 +0800

    使用cv API实现三种算法的特征提取

--- A1P1Q2_ORB.py
+++ A1P1Q2_ORB.py
@@ -8,18 +8,20 @@ img2 = cv.imread('src.jpg')
 gray1 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 gray2 = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 
-#进行SURF操作
-sift1 = cv.xfeatures2d.SIFT_create()
-sift2 = cv.xfeatures2d.SIFT_create()
-kp1,des1 = sift1.detectAndCompute(gray1,None)
-kp2,des2 = sift2.detectAndCompute(gray2,None)
+#进行ORB操作
+orb1 = cv.ORB_create()
+orb2 = cv.ORB_create()
+kp1 = orb1.detect(img1,None)
+kp2 = orb2.detect(img2,None)
+kp1, des1 = orb1.compute(img1, kp1)
+kp2, des2 = orb2.compute(img2, kp2)
 
 #绘制特征点
 cv.drawKeypoints(img1,kp1,img1,flags=0)
 cv.drawKeypoints(img2,kp2,img2,flags=0)
-plt.imshow(img1),plt.show()
-plt.imshow(img2),plt.show()
+plt.imshow(img1), plt.show()
+plt.imshow(img2), plt.show()
 
 #写文件
-cv.imwrite('dst_sift.jpg', img1)
-cv.imwrite('src_sift.jpg', img2)
+cv.imwrite('dst_org.jpg',img1)
+cv.imwrite('src_orb.jpg',img2)
--- A1P1Q2_SURF.py
+++ A1P1Q2_SURF.py
@@ -9,10 +9,10 @@ gray1 = cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 gray2 = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 
 #进行SURF操作
-sift1 = cv.xfeatures2d.SIFT_create()
-sift2 = cv.xfeatures2d.SIFT_create()
-kp1,des1 = sift1.detectAndCompute(gray1,None)
-kp2,des2 = sift2.detectAndCompute(gray2,None)
+surf1 = cv.xfeatures2d.SURF_create(400)
+surf2 = cv.xfeatures2d.SURF_create(400)
+kp1, des1 = surf1.detectAndCompute(gray1,None)
+kp2, des2 = surf2.detectAndCompute(gray2,None)
 
 #绘制特征点
 cv.drawKeypoints(img1,kp1,img1,flags=0)
@@ -21,5 +21,5 @@ plt.imshow(img1),plt.show()
 plt.imshow(img2),plt.show()
 
 #写文件
-cv.imwrite('dst_sift.jpg', img1)
-cv.imwrite('src_sift.jpg', img2)
+cv.imwrite('dst_surf.jpg', img1)
+cv.imwrite('src_surf.jpg', img2)
